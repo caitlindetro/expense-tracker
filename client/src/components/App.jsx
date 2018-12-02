02be1d96b1a78aac0f6aab6ff0f03f17b396b483
Caitlin Detro <caitlindetro@gmail.com>
2018-12-01 18:23:15 -0600

    React App rendering locally

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,21 +1,36 @@
 import React from 'react';
 import styled from 'styled-components';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css?family=Lobster');
+
+body {
+  background-color: "D5D5D5";
+  padding: 0;
+  margin: 0;
+  font-family: 'Lobster', cursive;
+}
+`;
+
+const Title = styled.h1`
+  color: #9099A2;
+`;
 
 class App extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       month: ''
     };
   }
 
-  var Title = styled.h1`
-    color: 'white';
-  `;
-
   render() {
     return (
-      <Title>PocketChange</Title>
+      <div>
+        <GlobalStyle />
+        <h1>PocketChange</h1>
+      </div>
     )
   }
 }
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,19 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { injectGlobal } from 'styled-components';
 import App from './components/App.jsx';
 
 ReactDOM.render(<App />, document.getElementById('app'));
-
-// Global style
-// eslint-disable-next-line
-injectGlobal`
-  @import url('https://fonts.googleapis.com/css?family=Lobster');
-
-  body {
-    background-color: violet;
-    padding: 0;
-    margin: 0;
-    font-family: 'Lobster', cursive;
-  }
-`
